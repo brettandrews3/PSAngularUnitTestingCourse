047c58dcdaf9ba1b11aaebd5ab8ae9b24d36016c
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-06-14 16:38:42 -0500

    PS Unit Tests 3.5 - Mocking to Isolate Code
    This has an example of mocking and how to use Jasmine's built-in of( ) to trick the tests accordingly.

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,8 +1,11 @@
+import { of } from "rxjs/internal/observable/of";
+import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component"
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let HEROES;
+  let mockHeroService;    // mock service to call in component below
 
   beforeEach(() => {
     HEROES = [
@@ -11,9 +14,24 @@ describe('HeroesComponent', () => {
       {id:3, name: 'SobchakAmateur', strength: 19}
     ]
 
-    // Unit Testing 3.4: We're getting the squiggles because HeroesComponent
-    // requires an additional method that we're not testing right now. Next up,
-    // we'll look at how mocking works to resolve this error.
-    component = new HeroesComponent({});
+    // Here, we pass in an array of methods that we'll need to fake:
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+
+    // To make this component work, we need to pass in an object that looks
+    // like HeroService, which we defined as 'mockHeroService':
+    component = new HeroesComponent(mockHeroService);
+  })
+
+  describe('delete', () => {
+    it('should remove indicated hero from heroes list', () => {
+      // For this deleteHero below, review the PS Unit Tests 3.5 video,
+      // starting around 3:50 mark******
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2);
+    })
   })
 })
