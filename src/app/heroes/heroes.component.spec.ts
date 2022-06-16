61a7ee3da5a8e694ed8e9946d6e04eebcef5b2e2
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-06-16 10:59:23 -0500

    Component tests in progress

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,18 @@
+import { HeroesComponent } from "./heroes.component"
+/*
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+
+  beforeEach(() => {
+    HEROES = [
+      {id:1, name: 'SpiderDude', strength: 8},
+      {id:2, name: 'Wonder Woman', strength: 24},
+      {id:3, name: 'Superboy', strength: 55}
+    ]
+
+    component = new HeroesComponent();
+  })
+  // stuff
+})
+*/
