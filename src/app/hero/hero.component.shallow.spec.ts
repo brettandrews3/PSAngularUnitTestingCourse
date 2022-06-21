bd2baad784c94693a4b35f94253fced5f21b6323
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-06-21 16:38:12 -0500

    PS Unit Tests 4.3 - The TestBed
    This starts the series on shallow integration tests. There's lots of notes in this test file, and the unit test videos will need a second run-through on the TestsPractice2 branch. Useful, dense content.

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { HeroComponent } from "./hero.component"
+
+describe('HeroComponent (shallow tests)', () => {
+
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    // Create a special module just for testing purposes.
+    // configureTestingModule takes a single parameter that's an object;
+    // That object exactly matches the layout of when we create an app.module.
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent]
+    });
+
+    // This function tells TestBed to use the testing module above & construct HeroComponent
+    // createComponent() returns a ComponentFixture--a wrapper for a component used in testing.
+    fixture = TestBed.createComponent(HeroComponent)
+  });
+
+  // Test: check that HeroComponent gets the correct Hero:
+  it('should have the correct hero', () => {
+    // Act
+    fixture.componentInstance.hero = {id: 1, name: 'SuperDave', strength: 3};
+    fixture.detectChanges();
+
+    // Assert
+    expect(fixture.componentInstance.hero.name).toEqual('SuperDave');
+  })
+})
