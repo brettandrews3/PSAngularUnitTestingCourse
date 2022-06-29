b68534ada3677612a91952b7ad548775f2540415
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-06-29 17:01:31 -0500

    PS Unit Tests 4.7 - More Complex Shallow Integration

---
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -0,0 +1,16 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { HeroesComponent } from "./heroes.component"
+
+describe('HeroesComponent (shallow tests', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent]
+    })
+    fixture = TestBed.createComponent(HeroesComponent);
+  })
+})
+
+
+// PS Unit Tests 4.7 - More Complex Shallow Integration
