2b1187cc375106e2e29f12a190fbd2c140bc9c62
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-06-29 17:17:24 -0500

    PS Unit Tests 4.8 - Mocking an Injected Service

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,16 +1,39 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { of } from "rxjs/internal/observable/of";
+import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component"
 
-describe('HeroesComponent (shallow tests', () => {
+describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
 
   beforeEach(() => {
+    HEROES = [
+      {id:1, name: 'SpiderDude', strength: 8},
+      {id:2, name: 'Wonder Woman', strength: 24},
+      {id:3, name: 'Superboy', strength: 55}
+    ];
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent]
+      declarations: [HeroesComponent],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     fixture = TestBed.createComponent(HeroesComponent);
   })
+
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES))
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3);
+  })
 })
 
-
 // PS Unit Tests 4.7 - More Complex Shallow Integration
+// PS Unit Tests 4.8 - Mocking an Injected Service
