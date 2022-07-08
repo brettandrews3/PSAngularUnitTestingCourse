7c095d43d9984beaae17f5ffbbbf796781646901
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-07-08 15:25:15 -0500

    PS Unit Tests 5.5 - Using inject() Helper Fn
    I deleted the complex inject function that Eames demonstrates in the back half of the video. This video ends just as we're about to handle the Assert statement on ln 40.

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,19 +1,21 @@
-import { TestBed } from "@angular/core/testing"
+import { inject, TestBed } from "@angular/core/testing"
 import { HeroService } from "./hero.service"
 import { MessageService } from "./message.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 
 // PS Unit Tests 5.4 - Integration Testing of Services
+// PS Unit Tests 5.5 - Using inject() Helper Fn
 
 describe('HeroService', () => {
   let mockMessageService;
   let httpTestingController: HttpTestingController;
+  let service: HeroService;
 
   beforeEach(() => {
     mockMessageService = jasmine.createSpyObj(['add']);
 
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [ HttpClientTestingModule ],
       providers: [
         HeroService,
         {provide: MessageService, useValue: mockMessageService}
@@ -22,8 +24,20 @@ describe('HeroService', () => {
 
     // Get a handle to the HttpTestingController
     httpTestingController = TestBed.inject(HttpTestingController);
+    //service = TestBed.inject(HeroService);    - Using inject syntax in it() below
 
     // Get a handle to a service (example)
     //let msgSvc = TestBed.inject(MessageService);
+  })
+
+  // inject() takes in 2 parameters: array[] & the callback() => that is the normal test
+  describe('getHero', () => {
+    it('should call getHero() w/ the correct URL', () => {
+      // Act: call getHero()
+      service.getHero(4);
+
+      // Assert: test that the URL was correct
+
+    })
   })
 })
