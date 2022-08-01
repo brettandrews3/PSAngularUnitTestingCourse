38123daa669fbcc21a8a5cd0c94043aee574cf36
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-08-01 10:33:52 -0500

    PS Unit Tests 7.2 - Adding Async Code
    Added debounce() to save() in hero-detail.component.ts to create an async situation. debounce() here applies a 250 ms delay to save(), meaning the user cannot spam the SAVE button successfully.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -32,8 +32,32 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  // 7.2: Adding in debounce fn described below; calling code for the save(),
+  // instead of the usual callback fn. We tell save() to wait 250ms before
+  // pinging the server. 'false' tells debounce() that we don't want to execute
+  // immediately.
  save(): void {
+  debounce(() => {
     this.heroService.updateHero(this.hero)
       .subscribe(() => this.goBack());
-  }
+  }, 250, false)();
 }
+
+// PS Unit Tests 7.2 - Adding Async Code
+
+// 7.2: Adding in a debounce fn to prevent bunch of server requests in case
+// user spams the Save button
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
